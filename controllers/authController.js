74227e700d3e22e13ff816a6a3139707b05d37a2
rishabh3562 <rishabh.dubey.cs21@ggits.net>
2025-11-21 11:02:44 +0530

    auth 1 p5 : register controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,23 +3,29 @@ const User = require("../model/userModel");
 const loginHandler = (req, res) => {
 }
 const registerHandler = async (req, res) => {
-    // 1
-    const { name, age, email, password, role } = req.body;
-    // 3
-    const data = new User({
-        name: name,
-        age: age,
-        email: email,
-        password: password,
-        role: role,
-    })
-    const savedUser = await data.save();
-    // 4
-    res.json({
-        success: true,
-        data: savedUser
-    })
-
+    try {
+        // 1
+        const { name, age, email, password, role } = req.body;
+        // 3
+        const data = new User({
+            name: name,
+            age: age,
+            email: email,
+            password: password,
+            role: role,
+        })
+        const savedUser = await data.save();
+        // 4
+        res.json({
+            success: true,
+            data: savedUser
+        })
+    } catch (err) {
+        res.json({
+            success: false,
+            error: err.message
+        })
+    }
 }
 const logoutHandler = (req, res) => {
 }
