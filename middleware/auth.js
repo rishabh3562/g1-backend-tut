858f726a3db0aeca0c9119a689ad8652fc1906c5
rishabh3562 <rishabh.dubey.cs21@ggits.net>
2025-11-26 13:51:51 +0530

    protect route

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,11 @@
 const jwt = require('jsonwebtoken');
+const User = require('../model/userModel');
 const generateToken = (payload) => {
     let token = jwt.sign(payload, process.env.JWT_SECRET);
     return token
 }
 
-const protect = (req, res, next) => {
+const protect = async (req, res, next) => {
     console.log("=====protect middleware=====")
 
     // token exists or not
@@ -22,8 +23,32 @@ const protect = (req, res, next) => {
     }
 
     // token -> _id -> user information 
+    try {
+        let payload = jwt.verify(token, process.env.JWT_SECRET);
+        if (!payload) {
+            res.json({
+                success: false,
+                message: "token is invlaid"
+            })
+        }
 
-    // req.user =user(token);
-    next();
+
+        let user = await User.findById(payload._id);
+        if (!user) {
+            res.json({
+                success: false,
+                message: "user not found"
+            })
+        }
+        req.user = user;
+        next();
+    } catch (error) {
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+
+  
 }
 module.exports = { generateToken, protect };
