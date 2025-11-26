df2b59c29ebf1abf918abcd6630d90c0ebf9e2ce
rishabh3562 <rishabh.dubey.cs21@ggits.net>
2025-11-26 13:45:36 +0530

    token check protect middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,5 +6,24 @@ const generateToken = (payload) => {
 
 const protect = (req, res, next) => {
     console.log("=====protect middleware=====")
+
+    // token exists or not
+    let token;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        token = req.header.authorization.split(" ")[1] // ["Bearer"," dhfkdasfbakjsdbfadsf"]
+    }
+
+    // if token exists then proceed
+    if (!token) {
+        return res.json({
+            success: false,
+            message: "token not found!"
+        })
+    }
+
+    // token -> _id -> user information 
+
+    // req.user =user(token);
+    next();
 }
 module.exports = { generateToken, protect };
