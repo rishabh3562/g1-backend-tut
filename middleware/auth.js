436e0ce98ac3076a235369fdfd1fd7529c48c7bd
rishabh3562 <rishabh.dubey.cs21@ggits.net>
2025-11-28 10:26:46 +0530

    authorize

--- middleware/auth.js
+++ middleware/auth.js
@@ -51,4 +51,17 @@ const protect = async (req, res, next) => {
 
 
 }
-module.exports = { generateToken, protect };
+// Grant access to specific roles
+const authorize = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            return res.status(403).json({
+                success: false,
+                message: `User role '${req.user.role}' is not authorized to access this route`
+            });
+        }
+        next();
+    };
+};
+
+module.exports = { generateToken, protect, authorize };
--- server.js
+++ server.js
@@ -4,11 +4,17 @@ require('dotenv').config();
 const app = express();
 const userRouter = require("./routes/userRoutes");
 const authRouter = require('./routes/authRoutes');
+const { authorize, protect } = require('./middleware/auth');
 
 app.use(express.json());
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/auth", authRouter)
-
+app.get('/admin', protect, authorize('admin'), (req, res) => {
+    res.send({
+        success: true,
+        message: "addmin only route"
+    })
+})
 app.listen(8000, () => {
     connectDB();
     console.log("server started")
